cacb05c1b57b75ebffad8b0dd105361f19bcdb36
Adam Becker <adambbecker@gmail.com>
2014-05-20 11:44:45 -0400

    Added common uses for router template

--- templates/router.js
+++ templates/router.js
@@ -7,6 +7,33 @@
 
     <%= _.classify(themeTitle) %>.Routers.<%= _.classify(name) %> = Backbone.Router.extend({
 
+      routes: {
+        '*notFound' : 'go',
+			  ''          : 'go'
+      },
+
+      go: function(pathname) {
+        var url = '/';
+
+  			if ( !_.isNull( pathname ) ) {
+  				url += pathname
+  			}
+
+        if (url.indexOf('?') > 0) {
+          url = url.replace(/\?/, '?json=1&');
+        } else {
+          url += '?json=1';
+        }
+
+        $.get(url)
+        .done(function(data) {
+          // Set stuff with data
+        })
+        .fail(function(err) {
+          console.warn('error', err);
+        })
+      }
+
     });
 
 })();
